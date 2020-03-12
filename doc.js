12c9a7a6e1bf1a771bf0ff0c7220dc3ec6bdd43f
Panther <marudhupandiyang@gmail.com>
2020-03-12 16:19:43 +0530

    update initial

--- doc.js
+++ doc.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
+const copyfiles = require('copyfiles');
 
 let templatePath = '';
 let templateData = {};
@@ -18,14 +19,19 @@ async function generateFiles(data, template) {
   templatePath = getTemplatePath(template);
   templateData = data;
 
-  await generateRoot();
+  // Copy
+  const paths = [path.resolve(templatePath, 'public/**')];
+  paths.push(outputPath);
+  copyfiles(paths, {}, async () => {
+    await generateRoot();
+  });
 }
 
 async function generateRoot() {
-  const fileName = '/index.html';
+  const fileName = '/index.html.ejs';
 
   const renderedHtml = await ejs.renderFile(`${templatePath}${fileName}`, templateData, ejsOptions);
-  fs.writeFileSync(`${outputPath}${fileName}`, renderedHtml);
+  fs.writeFileSync(`${outputPath.replace('ejs', '')}${fileName}`, renderedHtml);
 }
 
 module.exports = generateFiles;
